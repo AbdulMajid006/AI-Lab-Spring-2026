33d504d87210efb488319bd2be86a8233689848c
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-26 20:05:57 +0500

    Create Task1.py

---
+++ Lab 2/Task1.py
@@ -0,0 +1,20 @@
+# Task#1:
+# Student Profile System (Basics of Class & Object):
+# Create a Student class with attributes name, roll_no, and program.
+# Add a method display_info() to show student details.
+# Create at least two student objects and display their information.
+
+class Student:
+    def __init__(self, name, roll_no, program):
+        self.name = name
+        self.roll_no = roll_no
+        self.program = program
+
+    def display_info(self):
+        print(f"Name: {self.name}, Roll No: {self.roll_no}, Program: {self.program}")
+
+s1 = Student("Ali", 123, "CS")
+s2 = Student("Hammad", 421, "SE")
+
+s1.display_info()
+s2.display_info()
