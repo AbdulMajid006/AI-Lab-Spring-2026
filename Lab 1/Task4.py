0d6900963690fd1736e2ab87fd853e5379b2b67c
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-26 19:15:15 +0500

    Create Task4.py

---
+++ Lab 1/Task4.py
@@ -0,0 +1,12 @@
+# Create a tuple of numbers.
+# Use a loop to:
+# • Print each element
+# • Print the total sum of tuple elements
+
+tuple1 = (1, 2, 3, 10, 98, 12)
+
+sum = 0
+for i in tuple1:
+    print(i)
+    sum += i
+print("The sum is: ", sum)
