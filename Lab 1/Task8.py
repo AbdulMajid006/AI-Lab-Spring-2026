34e8fc8d261a77c8f5897d7448362ab51d803113
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-26 19:17:58 +0500

    Create Task8.py

---
+++ Lab 1/Task8.py
@@ -0,0 +1,10 @@
+# Using range() and a for loop:
+# • Print all even numbers between 1 and 50
+# • Count how many even numbers are printed
+
+count = 0
+for i in range(1, 50):
+    if i % 2 ==0:
+        print(i)
+        count += 1
+print("Total even numbers: ", count)
