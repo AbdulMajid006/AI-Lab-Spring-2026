827c231f8e27a2d4675017725718bda5013937ba
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-26 19:19:14 +0500

    Implement functions for max and key-value printing
    Added functions to find maximum number and print key-value pairs.

---
+++ Lab 1/Task10.py
@@ -0,0 +1,18 @@
+# Create a function that:
+# • Accepts variable-length arguments
+# • Returns the maximum number
+# Create another function that:
+# • Accepts keyword arguments
+# • Prints them in key : value format
+
+def findmax(*args):
+    return max(args)
+
+def printkeyvalues(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key} : {value}")
+
+maxnumber = findmax(9, 50, 66, 86, 71)
+print("Maximum number:", maxnumber)
+
+printkeyvalues(name="Abdul Majid", age=20, department="Computer Science")
