f6b5dc8a2c9d1c7f43809cb5b2f1caeb380e276c
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-26 19:14:33 +0500

    Create Task3.py

---
+++ Lab 1/Task3.py
@@ -0,0 +1,13 @@
+# Create a list containing mixed data types.
+# Perform the following:
+# • Replace the second element
+# • Append a new element
+# • Remove the last element
+# • Print the final list
+
+list = [1, 2, 10, 12.33, 34.77, "Jack", "c"]
+list[1] = "Henry"
+list.append("Alex")
+list.pop()
+
+print("Final List: ",list)
