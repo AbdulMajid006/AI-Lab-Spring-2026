a132132928768d6a45d82a83f5bad9ff6ffbeb4f
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-26 19:15:55 +0500

    Create Task5.py

---
+++ Lab 1/Task5.py
@@ -0,0 +1,17 @@
+# Take 10 numbers from the user and store them in a list.
+# Create a set from that list and display:
+# • Original list
+# • Set
+# • Number of duplicate values removed
+
+list = []
+for i in range(10):
+    num = int(input("Enter a number: "))
+    list.append(num)
+print("List: ", list)
+set = set(list)
+
+print("The Set is: ", set)
+
+diff = len(list) - len(set)
+print("Number of duplicate values removed: ", diff)
