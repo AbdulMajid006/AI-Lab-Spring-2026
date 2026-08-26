2cd3bf682f9db6dc765b17b9ee3820b961cc18c4
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-26 19:18:32 +0500

    Create Task9.py

---
+++ Lab 1/Task9.py
@@ -0,0 +1,11 @@
+# Write a program using a while loop that:
+# • Takes a number from the user
+# • Prints its multiplication table
+# • Stops when the multiplier reaches 10
+
+num = int(input("Enter a number: "))
+count = 1
+while (count < 10):
+    mul = num*count
+    print(num," x ", count, " = ", mul)
+    count += 1
