68060a885be65941e8441b60be6028a165c987e4
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-26 19:16:38 +0500

    Implement student details dictionary with grading
    The program creates a student dictionary, assigns a grade based on marks, and prints the details.

---
+++ Lab 1/Task6.py
@@ -0,0 +1,23 @@
+# Create a dictionary to store student details:
+# • name
+# • marks
+# • grade
+# Using conditions:
+# • Assign grade based on marks
+# • Update the dictionary and print it
+
+student = {
+    "name": input("Enter student name: "),
+    "marks": int(input("Enter marks: "))
+}
+
+if student["marks"] >=80:
+    student["grade"] = "A+"
+elif student["marks"] >=70:
+    student["grade"] = "A"
+elif student["marks"] >=60:
+    student["grade"] = "B"
+else:
+    student["grade"] = "C"
+
+print("Student Details: ", student)
