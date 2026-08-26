614cc562f7111727c6c0a5c03a15b407a5640cc0
Abdul Majid <mkabdulmajid2006@gmail.com>
2026-08-26 19:12:56 +0500

    Create Task1.py to sum two numbers and show types
    This script takes two user-input numbers, calculates their sum, and prints both the sum and the data types of the input numbers.

---
+++ Lab 1/Task1.py
@@ -0,0 +1,14 @@
+# Task#1:
+# Take two numbers from the user.
+# Store them in variables and:
+# • Print their sum
+# • Print their data types 
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+sum = num1 + num2
+
+print("The result is: ", sum)
+print("Data Type of num1 is: ", type(num1))
+print("Data Type of num2 is: ", type(num2))
